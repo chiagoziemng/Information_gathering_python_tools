a75ca5c4666b43d663ec84fd23e833476f858d74
chiagoziemng <chiagoziemjamesng@gmail.com>
2024-08-01 22:30:10 +0100

    Fest: added DNS Enumeration

---
+++ dns_enumeration.py
@@ -0,0 +1,22 @@
+import dns.resolver
+
+# set the target domain and record type
+target_domain = "pfx.com.ng"
+record_types = [ "A", "AAAA", "CNAME", "MX", "NS", "SOA", "TXT"]
+
+# Create a DNS resolver
+resolver = dns.resolver.Resolver()
+for record_type in record_types:
+    # Perform DNS lookup for the targer domain and record type
+    try:
+        answers = resolver.resolve(target_domain, record_type)
+    except dns.resolver.NoAnswer:
+    
+        continue
+
+    # Print the DNS records found
+
+    print(f"DNS records for {target_domain} ({record_type}):")
+
+    for rdata in answers:
+        print(rdata)
